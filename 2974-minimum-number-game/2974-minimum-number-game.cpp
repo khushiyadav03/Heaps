44e70ada43bfb9be5340488cb988c86551b3f081
Khushi Yadav <ky0307005@gmail.com>
2026-01-16 17:54:59 +0530

    Time: 1 ms (33.12%), Space: 28.7 MB (7%) - LeetHub

--- 2974-minimum-number-game/2974-minimum-number-game.cpp
+++ 2974-minimum-number-game/2974-minimum-number-game.cpp
@@ -2,23 +2,21 @@ class Solution {
 public:
     vector<int> numberGame(vector<int>& nums) {
         priority_queue<int, vector<int>, greater<int>> pq;
-        
+
         // push all elements into min heap
-        for (int x : nums) {
+        for(int x : nums){
             pq.push(x);
         }
 
         vector<int> arr;
 
-        // play the game
-        while (!pq.empty()) {
+        while(!pq.empty()){
             int alice = pq.top();
             pq.pop();
 
             int bob = pq.top();
             pq.pop();
 
-            // Bob appends first, then Alice
             arr.push_back(bob);
             arr.push_back(alice);
         }
