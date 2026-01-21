043c1b036beb77acafe77e1275bc52aaa3d163e0
Khushi Yadav <ky0307005@gmail.com>
2026-01-21 15:01:47 +0530

    Time: 3 ms (28.23%), Space: 8.6 MB (20.09%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -4,9 +4,8 @@ public:
         int n = 0;
         while(x != 0){
             int digit = x % 10;
-
+            
             if((n > INT_MAX/10) || (n < INT_MIN/10)) return 0;
-
             n = n * 10 + digit;
             x = x / 10;
         }
